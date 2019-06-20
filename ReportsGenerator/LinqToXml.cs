25a8900f3892796dfc8785c6d15ff68e8373da91
achoi0330 <achoi0330@gmail.com>
2019-06-20 22:31:28 +1200

    Initial check-in of module ReportsGenerator

---
+++ ReportsGenerator/LinqToXml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ReportsGenerator
+{
+    public class LinqToXml
+    {
+        public void GenerateReports(string sourceFilePath, string destinationFilePath)
+        {
+            var root = XElement.Load(sourceFilePath);
+            var studentsList = GetStudentsInASpecificLocation(root, "Auckland");
+            WriteNamesToTextFile(studentsList, destinationFilePath);
+        }
+
+        private IEnumerable<XElement> GetStudentsInASpecificLocation(XElement root, string location)
+        {
+            var studentsList =
+                from el in root.Elements("Student")
+                where (string)el.Element("Location") == location
+                select el;
+
+            return studentsList;
+        }
+
+        private void WriteNamesToTextFile(IEnumerable<XElement> list, string destinationFilePath)
+        {
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(destinationFilePath))
+            {
+                foreach (XElement el in list)
+                {
+                    file.WriteLine($"{(string)el.Element("FirstName")} {(string)el.Element("LastName")}");
+                }
+            }
+        }
+
+
+    }
+}
---
+++ ReportsGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReportsGenerator
+{
+    class ReportsGenerator
+    {
+        const string sourceXmlFilePath = "/Users/anna/Documents/Students.xml";
+        const string destinationTxtFilePath = "/Users/anna/Documents/StudentsFormatted.txt";
+
+
+        public static void Main(string[] args)
+        {
+            new LinqToXml().GenerateReports(sourceXmlFilePath, destinationTxtFilePath);
+        }
+
+    }
+}
